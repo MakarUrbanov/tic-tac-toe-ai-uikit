0e5a90702493a66b3f30f2cab2bfc510f3b31144
Makar <84675052+MakarUrbanov@users.noreply.github.com>
2022-10-16 02:44:26 +0600

    init

---
+++ tic-tac-toe-ai-uikit/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  tic-tac-toe-ai-uikit
+//
+//  Created by makar on 10/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+
+}
+
